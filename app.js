ab47a44bd993671a7c6dec238ab17d6238351b58
WhalenSITHS <38401651+WhalenSITHS@users.noreply.github.com>
2020-02-27 09:25:15 -0500

    nested objects

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 /////////////////////Objects
-const Lisa = {
+/* const Lisa = {
   name: "Lisa",
   age: 17,
   graduated: false
 };
 console.log(Lisa.name);
+
+Math.random;
+Array.from;
+Object.create();
+Map.apply();
+ */
+
+//console.log(Math);
+
+/* const Homer = {
+  name: "Homer",
+  age: 32,
+  graduated: false,
+  family: {
+    wife: {
+      name: "Marge",
+      age: 32
+    },
+    son: {
+      name: "bart",
+      age: 10
+    },
+    daughter: {
+      name: "Lisa",
+      age: 8
+    }
+  }
+};
+
+console.log(Homer);
+ */
