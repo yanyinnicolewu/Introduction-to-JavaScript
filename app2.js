3026ab8857725840c8ffeb072a50b482e608a1a4
yanyinnicolewu <nicolewu288@gmail.com>
2025-09-26 09:34:11 -0700

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,9 +1,18 @@
-function enter(haskey, hasMagic, isBanned) {
-  if ((haskey === true || hasMagic === true) && !isBanned) {
-    console.log("you're princess is in another castle");
-  } else {
-    console.log("cannot enter");
-  }
-}
+// function enter(haskey, hasMagic, isBanned) {
+//   if ((haskey === true || hasMagic === true) && !isBanned) {
+//     console.log("you're princess is in another castle");
+//   } else {
+//     console.log("cannot enter");
+//   }
+// }
 
-enter(true, false, false);
+// function enter(rideHeightOK, withAdult, healthHold) {
+//   if ((rideHeightOK == true || withAdult == true) && !healthHold) {
+//     console.log("You are allowed");
+//   } else {
+//     console.log("You are not allowed");
+//   }
+// }
+// enter(true, false, false);
+
+function enter(small, medium, large)
