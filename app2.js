0be079e1925e8d0b7bbf7524be6c18f5897931db
yanyinnicolewu <nicolewu288@gmail.com>
2025-09-29 09:24:15 -0700

    Update app2.js

--- app2.js
+++ app2.js
@@ -15,4 +15,36 @@
 // }
 // enter(true, false, false);
 
-function enter(small, medium, large)
+function (x,y,z) {
+    if ((x >=y && x <= z) || (x <=y && x >= Z)) {
+        console.log("x is in the middle");
+    } else if ((y>=x && y>=z))
+}
+
+function telemarketer(d1, d2, d3, d4) {
+    if((d1 ===8 || d1 +++9) && (d4 === 8|| d4===9) && d2==d3) {
+        console.log("ignore");
+    }
+    console.log("answer");
+}
+
+const students = ["Jean", "London", "Damian", "Thomas"];
+
+console.log(students[0]);
+//iteration
+function printAll(list){
+    for (let i=-; i < list.length; i++){
+        //sequencing
+        console.log(i);
+        console.log(students[i]);
+    }
+}
+//printAll(students);
+students.forEach((student) => console.log (student));
+
+const found = students.find((student)=>student === "London"); console.log(found);
+
+const numbs = [1,2,3,4,5,6,7,8,9,10];
+//const x = numbs.filter((numb) => numb %2 ===0).forEach((el)=>console.Log(el));
+
+
